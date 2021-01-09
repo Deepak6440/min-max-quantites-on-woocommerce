eed523dccbdfde90ebaafd7e77613e3d892242ca
Deepak6440 <59681582+Deepak6440@users.noreply.github.com>
2021-01-09 23:07:29 +0530

    Create min-max-quantites.php

---
+++ min-max-quantites.php
@@ -0,0 +1,138 @@
+<?php 
+/*
+    Plugin Name: Min Max Quantites For Woocommerce
+    Plugin URI: https://4waytechnologies.com/
+    Description: This plugin is use for to manage the min max products quantites from backend and show the exact quantites on single product page.
+    Version:2.1.0
+    Author: Deepak kumar gupta
+    Author URI: https://4waytechnologies.com/
+ */
+ 
+ /*
+* Changing the minimum quantity to 2 for all the WooCommerce products
+*/
+
+function woocommerce_quantity_input_min_callback( $min, $product ) {
+	$min = 10;  
+	return $min;
+}
+add_filter( 'woocommerce_quantity_input_min', 'woocommerce_quantity_input_min_callback', 10, 2 );
+
+/*
+* Changing the maximum quantity to 5 for all the WooCommerce products
+*/
+
+function woocommerce_quantity_input_max_callback( $max, $product ) {
+	$max = 720;  
+	return $max;
+}
+add_filter( 'woocommerce_quantity_input_max', 'woocommerce_quantity_input_max_callback', 10, 2 );
+
+// To Create Backend Field In Min Max Quantity For Each Products
+
+function wc_qty_add_product_field() {
+
+	echo '<div class="options_group">';
+	woocommerce_wp_text_input( 
+		array( 
+			'id'          => '_wc_min_qty_product', 
+			'label'       => __( 'Minimum Quantity', 'woocommerce-max-quantity' ), 
+			'placeholder' => '',
+			'desc_tip'    => 'true',
+			'description' => __( 'Optional. Set a minimum quantity limit allowed per order. Enter a number, 1 or greater.', 'woocommerce-max-quantity' ) 
+		)
+	);
+	echo '</div>';
+
+	echo '<div class="options_group">';
+	woocommerce_wp_text_input( 
+		array( 
+			'id'          => '_wc_max_qty_product', 
+			'label'       => __( 'Maximum Quantity', 'woocommerce-max-quantity' ), 
+			'placeholder' => '',
+			'desc_tip'    => 'true',
+			'description' => __( 'Optional. Set a maximum quantity limit allowed per order. Enter a number, 1 or greater.', 'woocommerce-max-quantity' ) 
+		)
+	);
+	echo '</div>';	
+}
+add_action( 'woocommerce_product_options_inventory_product_data', 'wc_qty_add_product_field' );
+
+// Save min and max quantity on backend
+
+/*
+* This function will save the value set to Minimum Quantity and Maximum Quantity options
+* into _wc_min_qty_product and _wc_max_qty_product meta keys respectively
+*/
+
+function wc_qty_save_product_field( $post_id ) {
+	$val_min = trim( get_post_meta( $post_id, '_wc_min_qty_product', true ) );
+	$new_min = sanitize_text_field( $_POST['_wc_min_qty_product'] );
+
+	$val_max = trim( get_post_meta( $post_id, '_wc_max_qty_product', true ) );
+	$new_max = sanitize_text_field( $_POST['_wc_max_qty_product'] );
+	
+	if ( $val_min != $new_min ) {
+		update_post_meta( $post_id, '_wc_min_qty_product', $new_min );
+	}
+
+	if ( $val_max != $new_max ) {
+		update_post_meta( $post_id, '_wc_max_qty_product', $new_max );
+	}
+}
+add_action( 'woocommerce_process_product_meta', 'wc_qty_save_product_field' );
+
+/*
+* Setting minimum and maximum for quantity input args. 
+*/
+
+function wc_qty_input_args( $args, $product ) {
+	
+	$product_id = $product->get_parent_id() ? $product->get_parent_id() : $product->get_id();
+	
+	$product_min = wc_get_product_min_limit( $product_id );
+	$product_max = wc_get_product_max_limit( $product_id );	
+
+	if ( ! empty( $product_min ) ) {
+		// min is empty
+		if ( false !== $product_min ) {
+			$args['min_value'] = $product_min;
+		}
+	}
+
+	if ( ! empty( $product_max ) ) {
+		// max is empty
+		if ( false !== $product_max ) {
+			$args['max_value'] = $product_max;
+		}
+	}
+
+	if ( $product->managing_stock() && ! $product->backorders_allowed() ) {
+		$stock = $product->get_stock_quantity();
+
+		$args['max_value'] = min( $stock, $args['max_value'] );	
+	}
+
+	return $args;
+}
+add_filter( 'woocommerce_quantity_input_args', 'wc_qty_input_args', 10, 2 );
+
+function wc_get_product_max_limit( $product_id ) {
+	$qty = get_post_meta( $product_id, '_wc_max_qty_product', true );
+	if ( empty( $qty ) ) {
+		$limit = false;
+	} else {
+		$limit = (int) $qty;
+	}
+	return $limit;
+}
+
+function wc_get_product_min_limit( $product_id ) {
+	$qty = get_post_meta( $product_id, '_wc_min_qty_product', true );
+	if ( empty( $qty ) ) {
+		$limit = false;
+	} else {
+		$limit = (int) $qty;
+	}
+	return $limit;
+}
